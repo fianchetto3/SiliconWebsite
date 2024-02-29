52c0972ab526716aca5210bf22dc1c5143e0c8e1
simon <simonelenbrink@gmail.com>
2024-02-29 14:50:19 +0100

    Add project files.

---
+++ SiliconWebbApp/Models/Sections/ShowcaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SiliconWebbApp.Models.Sections
+{
+    public class ShowcaseViewModel
+    {
+        public string? Id { get; set; }
+
+        public string? Title { get; set; }
+        public string? Description { get; set; } 
+    }
+}
