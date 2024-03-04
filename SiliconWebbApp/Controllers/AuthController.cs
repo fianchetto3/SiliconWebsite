9a70dba5189fded8bdb09b417b92ab0e2e3b0599
simon <simonelenbrink@gmail.com>
2024-03-04 15:45:15 +0100

    AuthController Created

---
+++ SiliconWebbApp/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconWebbApp.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
