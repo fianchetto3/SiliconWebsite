239fba8cf5cd02e1f2ff9a029c0dea5e2fdb6030
simon <simonelenbrink@gmail.com>
2024-03-05 17:51:01 +0100

    Signup functions added

--- SiliconWebbApp/Controllers/AuthController.cs
+++ SiliconWebbApp/Controllers/AuthController.cs
@@ -6,9 +6,22 @@ namespace SiliconWebbApp.Controllers;
 public class AuthController : Controller
 {
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
     {
         var viewModel = new SignUpViewModel();
         return View(viewModel);
+    }
+
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        return RedirectToAction("SignIn", "Auth");
+        
+       
     }
 }
--- SiliconWebbApp/Models/Form/SignUpModel.cs
+++ SiliconWebbApp/Models/Form/SignUpModel.cs
@@ -18,11 +18,13 @@ public class SignUpModel
 
     [DataType(DataType.EmailAddress)]
     [Display(Name = "Email", Prompt = "Enter your Email", Order = 2)]
+    [Required(ErrorMessage = "Invalid Email")]
     [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$ ", ErrorMessage = "Invalid Email")]
     public string Email { get; set; } = null!;
 
     [Display(Name = "Password", Prompt = "Enter your Password", Order = 3)]
     [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Invalid Password")]
     [RegularExpression("^(?=.*[a-zA-Z])(?=.*\\d).{8,}$" , ErrorMessage ="Invalid Password")]
     public string Password { get; set; } = null!;
 
@@ -30,13 +32,22 @@ public class SignUpModel
 
     [Display(Name = "Confirm Password", Prompt = "Confirm your password", Order = 4)]
     [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password must be confirmed")]
     [Compare(nameof(Password), ErrorMessage ="Password does not match" )]
     public string ConfirmPassword { get; set; } = null!;
 
     [Display(Name = "I agree to the Terms & Conditions", Prompt = "Enter your first name", Order = 5)]
-    [Required(ErrorMessage = "Invalid" )]
+    [CheckBoxRequired(ErrorMessage = "You must accept Terms & Conditions." )]
     public bool TermsAndCondition { get; set; } = false;
 
 
+
+}
+public class CheckBoxRequired : ValidationAttribute 
+{
+    public override bool IsValid(object? value)
+    {
+        return value is bool b && b;
+    }
 
 }
