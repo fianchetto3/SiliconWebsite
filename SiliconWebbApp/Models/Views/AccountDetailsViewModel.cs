77c68c36e20f3b35ac9fe2de53842b1892bc583f
simon <simonelenbrink@gmail.com>
2024-03-12 14:38:24 +0100

    account-details completed

---
+++ SiliconWebbApp/Models/Views/AccountDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SiliconWebbApp.Models.Account;
+
+namespace SiliconWebbApp.Models.Views;
+
+public class AccountDetailsViewModel
+{
+    public string Title { get; set; } = "Account Details";
+
+    public AccountDetailsBasicInfo BasicInfo { get; set; } = new AccountDetailsBasicInfo()
+    {
+        ProfileImage = "/images/john-doe.svg",
+        FirstName = "John",
+        LastName = "Doe",
+        Email = "john.doe@example.com"
+
+    };
+
+
+    public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
+
+
+}
